f0960b166eeed0c2892e5d61520d7b57077d6071
Kushal Patel <kushal300603@gmail.com>
2024-04-30 21:53:04 +0530

    Add handleError function to utils package

--- database/db.go
+++ database/db.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"github.com/KushalP47/CSE542-Blockchain-Project/pkg/utils"
+	"github.com/dgraph-io/badger/v3"
+)
+
+func InitDB() (*badger.DB, error) {
+	opts := badger.DefaultOptions("/tmp/badger")
+	db, err := badger.Open(opts)
+	utils.HandleError(err)
+	return db, err
+}
+
+func CloseDB(db *badger.DB) {
+	err := db.Close()
+	utils.HandleError(err)
+}
