0f2126ca58f44174f8fca2c0dbe250000ee89c53
Kushal Patel <kushal300603@gmail.com>
2024-05-01 15:47:04 +0530

    Remove unused import statements and empty package declaration

--- main.go
+++ main.go
@@ -1,24 +1,6 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/KushalP47/CSE542-Blockchain-Project/blockchain"
-)
+import ()
 
 func main() {
-	accountState := blockchain.InitAccounts()
-	accountState.AddAccount("Alice", 100)
-	accountState.AddAccount("Bob", 50)
-	accountState.AddAccount("Charlie", 75)
-	accountState.AddAccount("David", 25)
-	fmt.Println(accountState)
-
-	for _, account := range accountState.Accounts {
-		fmt.Printf("Account: %s\n", account.Name)
-		fmt.Printf("Address: %x\n", account.Address)
-		fmt.Printf("Nonce: %d\n", account.Nonce)
-		fmt.Printf("Balance: %d\n", account.Balance)
-		fmt.Println()
-	}
 }
