713f39dca8e32d57809797b1435f6a398e3d8222
Kushal Patel <kushal300603@gmail.com>
2024-05-03 14:13:04 +0530

    env added

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,1 +1,23 @@
 package blockchain
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type Header struct {
+	ParentHash       common.Hash
+	Miner            common.Address
+	StateRoot        common.Hash
+	TransactionsRoot common.Hash
+	Difficulty       uint64
+	TotalDifficulty  uint64
+	Number           uint64
+	Timestamp        uint64
+	ExtraData        []byte
+	Nonce            uint64
+}
+
+type Block struct {
+	Header       Header
+	Transactions []SignedTx
+}
